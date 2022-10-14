efc2029b8a085ff527d439cd8ade7b1b82e844a7
RABI <thaparabi8848@gmail.com>
2022-10-14 12:02:10 +0545

    first

---
+++ Week 1/task_04.py
@@ -0,0 +1,6 @@
+matchPlayed= 609
+batted= 1014
+notOut= 162
+totalRun= 48426
+avg= totalRun/(batted-notOut)
+print("The Average is ",avg)
---
+++ Week 1/task_05.py
@@ -0,0 +1,15 @@
+A=113
+B=175
+C=12
+a= 113//24
+b= 175//24
+c= 12//24
+
+mytuple=(a,b,c)
+for i in mytuple:
+    if i>0:
+        print("full")
+    else:
+        print("le over")
+
+
