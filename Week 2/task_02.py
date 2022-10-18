8b34574a3d17a30b2431a708954389a30f2a6eb3
RABI <thaparabi8848@gmail.com>
2022-10-18 17:16:16 +0545

    small change in week 2

---
+++ Week 2/task_02.py
@@ -0,0 +1,10 @@
+total= 100
+
+total -= 1
+print("The total is",total)
+
+total *= 4
+print("The total is",total)
+
+total /= 2
+print("The total is ",total)
---
+++ Week 2/task_03.py
@@ -0,0 +1,5 @@
+total= 98.2
+count= 5
+average= total/5
+
+print("The average is ",average)
