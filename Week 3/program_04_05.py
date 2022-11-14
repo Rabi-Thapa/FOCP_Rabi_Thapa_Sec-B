a689587989c30e44ba9f00c461c9301d095100ea
RABI <thaparabi8848@gmail.com>
2022-11-14 08:52:06 +0545

    week 4 completed

--- Week 3/program_04_05.py
+++ Week 3/program_04_05.py
@@ -4,13 +4,16 @@ while True:
     password= input("Enter a New Password: ")
     conform= input("Conform Password: ")
 
-    if (password or conform) in BAD_PASSWORDS :
+    if password != conform:
+        print("password does't match")
+
+    elif (password or conform) in BAD_PASSWORDS :
         print("BAD PASSWORD.")
 
     elif (password== conform) and (len(password)>= 8 and len(password)<= 12) :
         print("Password Set.")
         break
-
+    
     else:
         print("Password  must of length of 8 to 12 character.")
 
